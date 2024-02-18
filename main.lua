cca2fc00908762ae28a88e0c942ffa6229de635f
BrakeDude <lockbrake@gmail.com>
2024-02-18 18:51:44 +0300

    Fixed DSS error

--- main.lua
+++ main.lua
@@ -588,7 +588,7 @@ local function OverridePause(self, player, hook, action)
 				end
 			end
 		end
-		return 0.75
+		return true
 	end
 end
 mod:AddCallback(ModCallbacks.MC_INPUT_ACTION, OverridePause, InputHook.IS_ACTION_PRESSED)
